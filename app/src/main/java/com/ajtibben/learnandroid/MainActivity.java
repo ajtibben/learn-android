429c7c8bb5b99670fa37580319a42efaec801d32
Andre Tibben <ajtibben@gmail.com>
2014-10-04 15:07:10 +0200

    Initial commit

---
+++ app/src/main/java/com/ajtibben/learnandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ajtibben.learnandroid;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
